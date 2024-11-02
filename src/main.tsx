a2d9e4401bc8a4254a156447a1f311167b8d540c
MrHunterBoi <m.svoboda.corp@gmail.com>
2024-11-02 12:05:47 +0200

    Fix using gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,23 @@ import '@mantine/core/styles.css';
 import { ModalsProvider } from '@mantine/modals';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
-import { projectConfig } from '../config';
 
-
-const router = createBrowserRouter([
-  {
-    path: projectConfig.baseRoute,
-    element: <App />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: projectConfig.baseRoute,
+//     element: <App />,
+//   },
+// ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider>
       <ModalsProvider>
-        <RouterProvider router={router} />
+        <HashRouter>
+          <App />
+        </HashRouter>
       </ModalsProvider>
     </MantineProvider>
   </StrictMode>
